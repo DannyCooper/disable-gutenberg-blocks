8ddbba19e44fd9c669aff734692e3ce13ad232d1
Danny Cooper <email@dannycooper.com>
2018-12-04 10:18:21 +0000

    Fix 'headers already sent' error

--- class-dgb-admin-page.php
+++ class-dgb-admin-page.php
@@ -20,6 +20,7 @@ class DGB_Admin_Page {
 	 */
 	public function __construct() {
 		// Register the submenu.
+		add_action( 'load-gutenberg_page_disable-blocks', array( $this, 'process_bulk_action' ) );
 		add_action( 'admin_menu', array( $this, 'register_sub_menu' ), 50 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ), 10 );
 	}
@@ -30,6 +31,10 @@ class DGB_Admin_Page {
 	 * @param string $hook The current page ID.
 	 */
 	public function enqueue( $hook ) {
+
+		if ( 'gutenberg_page_disable-blocks' !== $hook ) {
+			return;
+		}
 
 		wp_enqueue_style( 'dgb-admin', plugins_url( 'css/style.css', __FILE__ ), array(), '1.0.0' );
 		wp_add_inline_script(
@@ -69,7 +74,6 @@ class DGB_Admin_Page {
 	public function submenu_page_callback() {
 		$table = new DGB_List_Table();
 		$table->prepare_items();
-		$count = $table->get_row_count();
 		?>
 		<div class="wrap">
 			<h2><?php esc_html_e( 'Disable Gutenberg Blocks', 'disable-gutenberg-blocks' ); ?></h2>
@@ -88,6 +92,98 @@ class DGB_Admin_Page {
 				</div>
 			</div>
 			<?php
+	}
+
+	/**
+	 * [process_bulk_action description]
+	 */
+	public function process_bulk_action() {
+
+		// Detect when a bulk action is being triggered...
+		if ( isset( $_GET['action'] ) && 'disable' === $_GET['action'] ) {
+
+			// In our file that handles the request, verify the nonce.
+			$nonce = ( isset( $_REQUEST['_wpnonce'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ) : '' );
+			$block = ( isset( $_GET['block'] ) ? sanitize_text_field( wp_unslash( $_GET['block'] ) ) : '' );
+
+			if ( ! wp_verify_nonce( $nonce, 'dgb_nonce' ) ) {
+				die( 'Not today.' );
+			} else {
+				$this->disable_block( $block );
+				wp_safe_redirect( admin_url( 'admin.php?page=disable-blocks' ) );
+				exit();
+			}
+		}
+
+		// Detect when a bulk action is being triggered...
+		if ( isset( $_GET['action'] ) && 'enable' === $_GET['action'] ) {
+
+			// In our file that handles the request, verify the nonce.
+			$nonce = ( isset( $_REQUEST['_wpnonce'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ) : '' );
+			$block = ( isset( $_GET['block'] ) ? sanitize_text_field( wp_unslash( $_GET['block'] ) ) : '' );
+
+			if ( ! wp_verify_nonce( $nonce, 'dgb_nonce' ) ) {
+				die( 'Not today.' );
+			} else {
+				$this->enable_block( $block );
+			}
+
+			wp_safe_redirect( admin_url( 'admin.php?page=disable-blocks' ) );
+			exit();
+		}
+
+		// If the disable enable action is triggered.
+		if ( ( isset( $_POST['action'] ) && 'bulk-enable' === $_POST['action'] )
+		|| ( isset( $_POST['action2'] ) && 'bulk-enable' === $_POST['action2'] )
+		) {
+
+			$bulk_change_ids = ( isset( $_POST['bulk-change'] ) ? $_POST['bulk-change'] : array() );
+
+			// loop over the array of record IDs and enable them.
+			foreach ( $bulk_change_ids as $id ) {
+				$this->enable_block( $id );
+			}
+		}
+
+		// If the disable bulk action is triggered.
+		if ( ( isset( $_POST['action'] ) && 'bulk-disable' === $_POST['action'] )
+		|| ( isset( $_POST['action2'] ) && 'bulk-disable' === $_POST['action2'] )
+		) {
+
+			$bulk_change_ids = ( isset( $_POST['bulk-change'] ) ? $_POST['bulk-change'] : array() );
+
+			// loop over the array of record IDs and disable them.
+			foreach ( $bulk_change_ids as $id ) {
+				$this->disable_block( $id );
+			}
+		}
+	}
+
+	/**
+	 * [disable_block description]
+	 *
+	 * @param string $name Name of block to disable.
+	 */
+	public function disable_block( $name ) {
+		$blocks = (array) get_option( 'dgb_disabled_blocks', array() );
+		if ( ! in_array( $name, $blocks, true ) ) {
+			$blocks[] = $name;
+		}
+		update_option( 'dgb_disabled_blocks', $blocks );
+	}
+
+	/**
+	 * [disable_block description]
+	 *
+	 * @param string $name Name of block to enable.
+	 */
+	public function enable_block( $name ) {
+		$blocks     = (array) get_option( 'dgb_disabled_blocks', array() );
+		$new_blocks = array();
+		if ( in_array( $name, $blocks, true ) ) {
+			$new_blocks = array_diff( $blocks, array( $name ) );
+		}
+		update_option( 'dgb_disabled_blocks', $new_blocks );
 	}
 
 }
--- class-dgb-list-table.php
+++ class-dgb-list-table.php
@@ -89,9 +89,6 @@ class DGB_List_Table extends WP_List_Table {
 	 */
 	public function prepare_items() {
 
-		// Process bulk action.
-		$this->process_bulk_action();
-
 		$columns               = $this->get_columns();
 		$hidden                = array();
 		$sortable              = array();
@@ -163,102 +160,6 @@ class DGB_List_Table extends WP_List_Table {
 	}
 
 	/**
-	 * [process_bulk_action description]
-	 */
-	public function process_bulk_action() {
-
-		// Detect when a bulk action is being triggered...
-		if ( 'disable' === $this->current_action() ) {
-
-			// In our file that handles the request, verify the nonce.
-			$nonce = ( isset( $_REQUEST['_wpnonce'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ) : '' );
-			$block = ( isset( $_GET['block'] ) ? sanitize_text_field( wp_unslash( $_GET['block'] ) ) : '' );
-
-			if ( ! wp_verify_nonce( $nonce, 'dgb_nonce' ) ) {
-				die( 'Not today.' );
-			} else {
-				$this->disable_block( $block );
-				// esc_url_raw() is used to prevent converting ampersand in url to "#038;"
-				// add_query_arg() return the current url.
-				wp_safe_redirect( admin_url( 'admin.php?page=disable-blocks' ) );
-				exit();
-			}
-		}
-
-		// Detect when a bulk action is being triggered...
-		if ( 'enable' === $this->current_action() ) {
-
-			// In our file that handles the request, verify the nonce.
-			$nonce = ( isset( $_REQUEST['_wpnonce'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ) : '' );
-			$block = ( isset( $_GET['block'] ) ? sanitize_text_field( wp_unslash( $_GET['block'] ) ) : '' );
-
-			if ( ! wp_verify_nonce( $nonce, 'dgb_nonce' ) ) {
-				die( 'Not today.' );
-			} else {
-				$this->enable_block( $block );
-			}
-
-			// esc_url_raw() is used to prevent converting ampersand in url to "#038;"
-			// add_query_arg() return the current url.
-			wp_safe_redirect( admin_url( 'admin.php?page=disable-blocks' ) );
-			exit();
-		}
-
-		// If the disable enable action is triggered.
-		if ( ( isset( $_POST['action'] ) && 'bulk-enable' === $_POST['action'] )
-		|| ( isset( $_POST['action2'] ) && 'bulk-enable' === $_POST['action2'] )
-		) {
-
-			$bulk_change_ids = ( isset( $_POST['bulk-change'] ) ?  $_POST['bulk-change'] : array() );
-
-			// loop over the array of record IDs and enable them.
-			foreach ( $bulk_change_ids as $id ) {
-				$this->enable_block( $id );
-			}
-		}
-
-		// If the disable bulk action is triggered.
-		if ( ( isset( $_POST['action'] ) && 'bulk-disable' === $_POST['action'] )
-		|| ( isset( $_POST['action2'] ) && 'bulk-disable' === $_POST['action2'] )
-		) {
-
-			$bulk_change_ids = ( isset( $_POST['bulk-change'] ) ? $_POST['bulk-change'] : array() );
-
-			// loop over the array of record IDs and disable them.
-			foreach ( $bulk_change_ids as $id ) {
-				$this->disable_block( $id );
-			}
-		}
-	}
-
-	/**
-	 * [disable_block description]
-	 *
-	 * @param string $name Name of block to disable.
-	 */
-	public function disable_block( $name ) {
-		$blocks = (array) get_option( 'dgb_disabled_blocks', array() );
-		if ( ! in_array( $name, (array) $blocks, true ) ) {
-			$blocks[] = $name;
-		}
-		update_option( 'dgb_disabled_blocks', $blocks );
-	}
-
-	/**
-	 * [disable_block description]
-	 *
-	 * @param string $name Name of block to enable.
-	 */
-	public function enable_block( $name ) {
-		$blocks     = (array) get_option( 'dgb_disabled_blocks', array() );
-		$new_blocks = array();
-		if ( in_array( $name, (array) $blocks, true ) ) {
-			$new_blocks = array_diff( $blocks, array( $name ) );
-		}
-		update_option( 'dgb_disabled_blocks', $new_blocks );
-	}
-
-	/**
 	 * [disable_block description]
 	 *
 	 * @param string $name Name of block to check if disabled.
@@ -266,7 +167,7 @@ class DGB_List_Table extends WP_List_Table {
 	public function is_block_disabled( $name ) {
 		$disabled_blocks = (array) get_option( 'dgb_disabled_blocks', array() );
 
-		if ( in_array( $name, (array) $disabled_blocks, true ) ) {
+		if ( in_array( $name, $disabled_blocks, true ) ) {
 			return true;
 
 		}
